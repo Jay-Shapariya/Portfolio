9733018f422a6702c1ef6b94f91813a358f474b6
jay <jay@un-app.ai>
2024-11-28 10:44:33 +0530

    resume change

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -16,7 +16,7 @@ const About = () => {
                 </p>
                 <div className="flex gap-4 items-center justify-center lg:justify-start mt-8">
                 <button type="button" className="text-white bg-gradient-to-r from-cyan-500 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 font-medium rounded-lg text-3xl px-5 py-2.5 text-center me-2 mb-2"
-                onClick={() => window.open("/CV/jay-resume.pdf")}
+                onClick={() => window.open("/CV/Jay shapariya CV.pdf")}
               >
                 Download CV
               </button>
